4917aaacef27b71ac07f177b86d6929279e15871
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2013-05-22 15:35:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Urume - It's new $module
+
+# SYNOPSIS
+
+    use Urume;
+
+# DESCRIPTION
+
+Urume is ...
+
+# LICENSE
+
+Copyright (C) FUJIWARA Shunichiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
---
+++ lib/Urume.pm
@@ -0,0 +1,39 @@
+package Urume;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Urume - It's new $module
+
+=head1 SYNOPSIS
+
+    use Urume;
+
+=head1 DESCRIPTION
+
+Urume is ...
+
+=head1 LICENSE
+
+Copyright (C) FUJIWARA Shunichiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+FUJIWARA Shunichiro E<lt>fujiwara.shunichiro@gmail.comE<gt>
+
+=cut
+
