64d978d960d3aa21af56ece7ce03aa502225fee8
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2013-06-18 18:54:10 +0900

    ファイル更新をatomicに

--- lib/Urume/DnsmasqAgent.pm
+++ lib/Urume/DnsmasqAgent.pm
@@ -13,8 +13,8 @@ sub new {
     my $config = shift;
 
     my $self = {
-        redis   => $config->{redis},
-        config  => $config->{config} || "/etc/dnsmasq.d/urume.conf",
+        redis           => $config->{redis},
+        config_file     => $config->{config} || "/etc/dnsmasq.d/urume.conf",
         restart_command =>
             $config->{restart_command} || "/sbin/service dnsmasq restart",
     };
@@ -36,12 +36,16 @@ sub run {
         $channel => sub {
             my $message = shift;
             infof "message arrival: %s", $message;
-            open my $fh, ">", $self->{config} or do {
+            my $file = $self->{config_file};
+            my $tmp  = "$file.tmp";
+            open my $fh, ">", $tmp or do {
                 critf "Can't open %s: %s", $self->{config}, $!;
                 return;
             };
             $fh->print($message);
             $fh->close;
+            rename $tmp, $file
+                or critf "Can't rename %s to %s: %s", $tmp, $file, $!;
             system $self->{restart_command};
         }
     );
