ae4fa0b53fe3c766e4a4f88a3575e3a51bb29f2e
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2013-06-24 11:02:07 +0900

    server自身のDNS情報も提供

--- config.pl
+++ config.pl
@@ -1,7 +1,11 @@
 +{
     dns => {
         domain  => "localdomain",
-        servers => [ "8.8.8.8", "8.8.4.4" ],
+        servers => [ "127.0.0.1", "8.8.8.8", "8.8.4.4" ],
+    },
+    server => {
+        dns_name => "urume-server",
+        ip_addr  => "169.254.169.254",
     },
     mac_address_base => 0,
     base_images      => [],
